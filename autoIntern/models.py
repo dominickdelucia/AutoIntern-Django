780fe87ebd1c989b03edce17a338af612f087240
ant <ant@cs.columbia.edu>
2018-03-02 17:12:27 -0500

    This is a skeleton django app that works with our db. We need to take the credentials out of the source code

---
+++ autoIntern/models.py
@@ -0,0 +1,35 @@
+# Copyright 2015 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from django.db import models
+
+
+#Commenting this out as it is from the demo project. Keeping it in case it is useful
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+#
+#
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+
+class User(models.Model):
+    username = models.CharField(max_length=255, primary_key=True)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    group = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+# Create your models here.
