8ecc1046fb115370593feef34be25ba32b6fa3eb
Nick Puljic <nap2152@columbia.edu>
2018-03-04 21:09:43 -0500

    Added default testing database

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,7 @@
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -103,6 +104,21 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        # If you are using Cloud SQL for MySQL rather than PostgreSQL, set
+        # 'ENGINE': 'django.db.backends.mysql' instead of the following.
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '024lana',
+        # For MySQL, set 'PORT': '3306' instead of the following. Any Cloud
+        # SQL Proxy instances running locally must also be set to tcp:3306.
+        'PORT': '5432',
+    }
+
+
 # In the flexible environment, you connect to CloudSQL using a unix socket.
 # Locally, you can use the CloudSQL proxy to proxy a localhost connection
 # to the instance
@@ -116,6 +132,7 @@ if os.getenv('GAE_INSTANCE'):
     pass
 else:
     DATABASES['prod']['HOST'] = '127.0.0.1'
+
 # [END dbconfig]
 
 # Internationalization
