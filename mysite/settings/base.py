25fa103d0a662cd062b15e8bf38ba608d1ff15d9
ant <ant@cs.columbia.edu>
2018-03-05 18:37:51 -0500

    trying to break the dev app...want to see if it fixes the uat app. per Nick

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -106,7 +106,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '024lana',
+        # 'PASSWORD': '024lana',
+        'PASSWORD' : 'winnerwinner',
         # For MySQL, set 'PORT': '3306' instead of the following. Any Cloud
         # SQL Proxy instances running locally must also be set to tcp:3306.
         'PORT': '5432',
