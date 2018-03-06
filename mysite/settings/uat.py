33365b780481fa48062b3913a4529b5a38feeee7
ant <ant@cs.columbia.edu>
2018-03-05 21:56:45 -0500

    changed the host from a socket to localhost

--- mysite/settings/uat.py
+++ mysite/settings/uat.py
@@ -12,5 +12,6 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-DATABASES['default']['host'] = '/cloudsql/autointern-uat:us-east1:autointern-uat'
+# DATABASES['default']['host'] = '/cloudsql/autointern-uat:us-east1:autointern-uat'
+DATABASES['default']['host'] = '127.0.0.1'
 STATIC_URL = 'https://storage.googleapis.com/autointern-uat/static/'
