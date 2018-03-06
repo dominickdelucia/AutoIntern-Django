e57e757f0c79c04987e80a7a533ae92563db4ed4
Nick Puljic <nap2152@columbia.edu>
2018-03-06 15:25:42 -0500

    Update prod.py

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -103,7 +103,7 @@ DATABASES = {
     'default': {
         # If you are using Cloud SQL for MySQL rather than PostgreSQL, set
         # 'ENGINE': 'django.db.backends.mysql' instead of the following.
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'supersecret',
