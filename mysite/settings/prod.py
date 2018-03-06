42b7d776886e80c0eae6f96e0d9a2a46a581aee7
Nick Puljic <nap2152@columbia.edu>
2018-03-06 17:21:21 -0500

    Update prod.py

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -129,15 +129,15 @@ else:
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us'
 
-#TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC'
 
-#USE_I18N = True
+USE_I18N = True
 
-#USE_L10N = True
+USE_L10N = True
 
-#USE_TZ = True
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
