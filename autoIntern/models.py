b3dbeb9932fbb167978054227581a665eace9f11
Nick Puljic <nap2152@columbia.edu>
2018-03-20 20:11:25 -0400

    Deleting unused models

--- autoIntern/models.py
+++ autoIntern/models.py
@@ -1,30 +1,5 @@
-# Copyright 2015 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from django.db import models
 
-
-#Commenting this out as it is from the demo project. Keeping it in case it is useful
-# class Question(models.Model):
-#     question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField('date published')
-#
-#
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
 
 class User(models.Model):
     email = models.CharField(max_length=255, primary_key=True)
@@ -37,4 +12,3 @@ class User(models.Model):
 
     def getUserFromEmail(self, email):
         return User.objects.get(email=email)
-# Create your models here.
